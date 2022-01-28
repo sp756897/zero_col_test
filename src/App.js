47d3a97d512bd472e071ed1504f2b31559108e8d
sp756897 <akshaysp2090@gmail.com>
2022-01-28 18:45:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Routes, Route
+} from "react-router-dom";
+import Navbar from "./Navbar";
+import Landing from "./Landing";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Landing.js
@@ -0,0 +1,275 @@
+import React, { Component } from "react";
+class Landing extends Component {
+    constructor() {
+        super();
+        this.state = {
+            principal: "",
+            principal2: "",
+            rate: "",
+            rate2: "",
+            def: "",
+            loans: [],
+            defaultLoan: [],
+            total_profit: null,
+            total_profit_default: null
+        };
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+
+    onSubmitother = e => {
+        e.preventDefault();
+        this.setState({
+            loans: []
+        })
+        var loans = []
+        var principal = parseFloat(this.state.principal2)
+        var rate = parseFloat(this.state.rate2)
+        var def = parseFloat(this.state.def)
+        var collateral = principal
+        var i = 0
+        var total_interest = 0.0
+        var interest = 0.0
+        var previous_interest = 0.0
+        let topro = 0
+        while (def >= i) {
+            i += 1
+            console.log("Borrowing Loan no ", i)
+            console.log("Principal: ", principal, "Collateral: ", collateral)
+            interest = principal * (rate / 100)
+            total_interest += interest
+            loans.push({ "loan": i, "principal": principal, "collateral": collateral, "interest": interest, "total_interest": total_interest })
+            console.log("Repaying Loan: ")
+            console.log("Interest: ", interest, "Total_Interest: ", total_interest)
+
+            principal = principal + (interest / 3)
+            collateral = collateral - ((interest + previous_interest) / 3)
+            previous_interest = interest
+        }
+        topro = (total_interest + collateral) - principal
+
+        this.setState({
+            defaultLoan: loans,
+            total_profit_default: topro
+        })
+
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.setState({
+            defaultLoan: []
+        })
+        var loans = []
+        var principal = parseFloat(this.state.principal)
+        var rate = parseFloat(this.state.rate)
+        var collateral = principal
+        var i = 0
+        var total_interest = 0.0
+        var interest = 0.0
+        var previous_interest = 0.0
+
+        while (collateral > 0) {
+            i += 1
+            console.log("Borrowing Loan no ", i)
+            console.log("Principal: ", principal, "Collateral: ", collateral)
+            interest = principal * (rate / 100)
+            total_interest += interest
+            loans.push({ "loan": i, "principal": principal, "collateral": collateral, "interest": interest, "total_interest": total_interest })
+            console.log("Repaying Loan: ")
+            console.log("Interest: ", interest, "Total_Interest: ", total_interest)
+            principal = principal + (interest / 3)
+            collateral = collateral - ((interest + previous_interest) / 3)
+            previous_interest = interest
+        }
+        let topro = total_interest - (principal - (interest / 3))
+        this.setState({
+            loans: loans,
+            total_interest: topro
+        })
+
+    };
+
+    render() {
+        console.log(this.state.loans)
+        var allLoans = this.state.loans.map((val, key) => (
+            <div class="row" key={key}>
+                <div class="col s12 m9">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">Loan number {val.loan}</span>
+                            <p style={{ fontsize: "28px" }}>Principal: {val.principal} </p>
+                            <p>Collateral: {val.collateral} </p>
+                            <p>Interest: {val.interest} </p>
+                            <p>Total Interest: {val.total_interest} </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ))
+
+        var allloans2 = this.state.defaultLoan.map((val, key) => (
+            <div class="row" key={key}>
+                <div class="col s12 m9">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">Loan number {val.loan}</span>
+                            <p style={{ fontsize: "28px" }}>Principal: {val.principal} </p>
+                            <p>Collateral: {val.collateral} </p>
+                            <p>Interest: {val.interest} </p>
+                            <p>Total Interest: {val.total_interest} </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ))
+
+        return (
+            <div style={{ paddingTop: "10rem" }}>
+                <div class="row">
+                    <div class="col s5 push-s7">
+                        {allLoans}
+                        {allloans2}
+                    </div>
+                    <div class="col s7 pull-s4">
+                        <div class="row">
+                            <div class="col s12">
+                                <div class="row center-align" >
+                                    <div class="col">
+                                        <div class="card pink accent-3 center-align">
+                                            <div class="card-content white-text">
+                                                <span class="card-title">Loans Until Zero Collateral</span>
+                                                <div className="">
+                                                    <div className="row">
+                                                        <div className="col s8 offset-s2">
+                                                            <form noValidate onSubmit={this.onSubmit}>
+                                                                <div className="input-field col s12">
+                                                                    <input
+                                                                        onChange={this.onChange}
+                                                                        value={this.state.principal}
+                                                                        id="principal"
+                                                                        type="text"
+                                                                    />
+                                                                    <label htmlFor="principal">Principal</label>
+                                                                </div>
+                                                                <div className="input-field col s12">
+                                                                    <input
+                                                                        onChange={this.onChange}
+                                                                        value={this.state.rate}
+                                                                        id="rate"
+                                                                        type="number"
+                                                                    />
+                                                                    <label htmlFor="rate">Rate</label>
+                                                                </div>
+
+
+                                                                <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                                                                    <button
+                                                                        style={{
+                                                                            width: "150px",
+                                                                            borderRadius: "3px",
+                                                                            letterSpacing: "1.5px",
+                                                                            marginTop: "1rem"
+                                                                        }}
+                                                                        type="submit"
+                                                                        className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                                                    >
+                                                                        Calculate
+                                                                    </button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="valign-wrapper">
+                            <span style={{ fontSize: "21px" }}>
+                                {this.state.total_interest ?
+                                    "Total Net Profit: " + this.state.total_interest : ""}
+                            </span>
+                        </div>
+                        <div class="row">
+                            <div class="col m9">
+                                <div class="row center-align" >
+                                    <div class="col">
+                                        <div class="card purple accent-3 center-align">
+                                            <div class="card-content white-text">
+                                                <span class="card-title">Loan Default</span>
+                                                <div className="">
+                                                    <div className="row">
+                                                        <div className="col s8 offset-s2">
+                                                            <form noValidate onSubmit={this.onSubmitother}>
+                                                                <div className="input-field col s12">
+                                                                    <input
+                                                                        onChange={this.onChange}
+                                                                        value={this.state.principal2}
+                                                                        id="principal2"
+                                                                        type="number"
+                                                                    />
+                                                                    <label htmlFor="principal">Principal</label>
+                                                                </div>
+                                                                <div className="input-field col s12">
+                                                                    <input
+                                                                        onChange={this.onChange}
+                                                                        value={this.state.rate2}
+                                                                        id="rate2"
+                                                                        type="number"
+                                                                    />
+                                                                    <label htmlFor="rate">Rate</label>
+                                                                </div>
+                                                                <div className="input-field col s12">
+                                                                    <input
+                                                                        onChange={this.onChange}
+                                                                        value={this.state.def}
+                                                                        id="def"
+                                                                        type="number"
+                                                                    />
+                                                                    <label htmlFor="def">Default</label>
+                                                                </div>
+
+                                                                <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                                                                    <button
+                                                                        style={{
+                                                                            width: "150px",
+                                                                            borderRadius: "3px",
+                                                                            letterSpacing: "1.5px",
+                                                                            marginTop: "1rem"
+                                                                        }}
+                                                                        type="submit"
+                                                                        className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                                                    >
+                                                                        Calculate
+                                                                    </button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="valign-wrapper">
+                            <span style={{ fontSize: "21px" }}>
+                                {this.state.total_profit_default ?
+                                    "Total Net Profit: " + this.state.total_profit_default : ""}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Landing;
